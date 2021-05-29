05d223ecb11b090388db13f424a2f3103dd093ea
Li,Pin-Hsueh <pinxue.life@gmail.com>
2021-05-29 15:49:35 +0800

    Completed GetToken, ready to implement CheckType() funciton

--- main.cpp
+++ main.cpp
@@ -6,5 +6,10 @@ using namespace std;
 
 int main()
 {
-  cout << "Testing..." ;
+  Lexer *lx = new Lexer() ;
+  Token *r  ;
+  while( ( r = lx->GetToken()) != NULL )
+  {
+    cout << r->ToString() << endl ;
+  } //while()
 } // main()
